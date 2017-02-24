eadd87ff20c0255d4df2c2a05bf7e42a0916dbb6
Zenn <app.zenn@gmail.com>
2017-02-24 16:11:20 +0900

    loginページを作成

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,9 +1,12 @@
 class LineController < ApplicationController
   def login
-
+    channel_id = ENV['LINE_CHANNEL_ID']
+    redirect_uri = CGI.escape('http://localhost:3000/line/callback')
+    state = SecureRandom.hex
+    @login_url = "https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=#{channel_id}&redirect_uri=#{redirect_uri}&state=#{state}"
   end
 
   def callback
-
+    binding.pry
   end
 end
